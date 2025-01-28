ed08622bba6edfc6ad39b2fd0c331dd0ed67d2e4
Alessandro Cardenas-Engle <ci6117or@go.minnstate.edu>
2025-01-28 18:53:02 +0000

    Inital commit - Taco Hunger App

---
+++ Module3Lab/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+
+namespace TacoBurritoLab {
+    class Program {
+        static void Main(string[] args) {
+            //Display a welcome message and prompt the user for input
+            Console.WriteLine("Welcome to the Taco & Burrito Decision Maker!");
+            Console.WriteLine("How hungry are you (1-10)?");
+            int hungerLevel = int.Parse(Console.ReadLine());
+
+            // Decide what food to recommend based on the hunger level
+            if (hungerLevel >= 8) {
+                Console.WriteLine("You're super hungry! Order bith tacos and burritos");
+
+            }
+            else if (hungerLevel >= 5) {
+                Console.WriteLine("You're moderately hungry. Go for tacos!");
+
+            }
+            else {
+                Console.WriteLine("You're not that hungry. Opt for a burrito.");
+
+            } //end else if
+
+            // Display final food recommendation
+            String recommendation = (hungerLevel >= 5) ? "Tacos" : "Burritos";
+            Console.WriteLine($"I recommend: {recommendation}");
+
+            // Output a message to the user based on their hunger level
+            switch (hungerLevel) {
+                case 10: 
+                    Console.WriteLine("You're a burrito and taco champion!");
+                    break;
+                case 9:
+                case 8:
+                    Console.WriteLine("Taco Time!");
+                    break;
+                case 7:
+                case 6:
+                case 5:
+                    Console.WriteLine("Burrito it is!");
+                    break;
+                default:
+                    Console.WriteLine("Maybe just grab a snack");
+                    break;
+        
+            } // end switch
+        } //end main method
+    } //end Program class
+} //end Namespace
